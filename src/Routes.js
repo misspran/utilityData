882f86c73a1167e9341249be7dc7c360cb8191f2
Vi Tran <vipran@gmail.com>
2018-08-20 18:41:34 -0400

    whoops changed heading font to be visible

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ import Home from './Home';
 const Routes = ( ) =>
 <main>
 <Route exact path="/" component={Home} />
-<Route exact path="/data" component={App} />
+<Route path="/data" component={App} />
 </main>
 
 export default withRouter(Routes);
